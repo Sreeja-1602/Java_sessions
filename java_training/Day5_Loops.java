dd4c42304b0a3e3851f0888c86cf6f4608f15ff4
i am bharath reddy peddy <you@example.com>
2025-01-09 09:33:33 +0530

    first commit

---
+++ java_training/Day5_Loops.java
@@ -0,0 +1,11 @@
+class  Day5_Loops{
+    public static void main(String args[]){
+        for(int count=0; count <= 4;i++){
+            System.out.println(count);
+        }
+        for(int i=20;i<=35;i++){
+            System.out.println(i);
+        }
+    }
+
+}
---
+++ java_training/Main.java
@@ -0,0 +1,16 @@
+public class Main{
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        Main s = new Main();
+        s.show();
+    }
+    public static void show(){
+        System.out.println("Sreeja");
+
+    }
+
+}
+
