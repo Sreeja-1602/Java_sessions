396b406d09418d71bf15fedb5a52d26dcf2ed7ee
Sreeja-1602 <2311cs010479@mallareddyuniversity.ac.in>
2025-01-10 10:22:26 +0530

    Add files via upload

---
+++ SubString.java
@@ -0,0 +1,26 @@
+class SubString{
+    public static void main(String[] args){
+        public List<String> stringMatching(String[] words){
+            List<String> list = new ArrayList<String>();
+            for(int i =0;i<words.length;i++){
+                String stringOne = words[i];
+                for(int j=0;j<words.length;j++){
+                    String stringTwo = words[j];
+                if( i !== j && words[i].length() - sOne.length< stringTwo.length())
+                if(checkIfSubString(string One,string Two) == true){
+                    list.add(string.One);
+                }
+
+                }
+            }
+        }
+        return list; 
+    }
+           public boolean checkIfString (String sOne, string sTwo){
+            for(int i =0;i < sTwo.length();i++){
+                if(sOne.equals(sTwo.subString(i, sOne.length()+i)))
+            }
+           }
+           return false;
+    }
+}
