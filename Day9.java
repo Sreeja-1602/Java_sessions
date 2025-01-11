8606143b9b16afdef2b4d77cde0a02002bb8174c
Sreeja-1602 <2311cs010479@mallareddyuniversity.ac.in>
2025-01-11 11:24:06 +0530

    Add files via upload

---
+++ Day9.java
@@ -0,0 +1,16 @@
+class Day9{
+    int id;
+    String name = "SreejaREddy";
+    String branch;
+    public static void main(String[] args){
+        Day9 s = new Day9();
+        s.name ="Sreeja";
+        s.id = 98;
+        s.branch = "CSE";
+        System.out.println(name);
+        
+        
+        
+    }
+
+}
